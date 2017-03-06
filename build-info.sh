f77986c359f5d4f069466d3de031f253829cae0f
Jason Taylor <jason@jasongtaylor.com>
2017-03-05 21:53:32 -0500

    Add Jenkins build info

--- build-info.sh
+++ build-info.sh
@@ -3,7 +3,9 @@
 # Bash Script for Jenkins Build Information
 
 cat << EOF > build.txt
+Information for Project ${JOB_NAME} on Build ${BUILD_NUMBER}
 The Jenkins Home is ${JENKINS_HOME}
 The current directory is $(pwd)
 Jenkins is running as user ${USER} on $(uname -a)
 EOF
+cat build.txt
